4ae5ee281865b45990393405ddbf89da90f9156c
NicoGm1 <nicolassurget@hotmail.fr>
2024-01-17 09:26:56 +0100

    first commit

---
+++ src/test/java/fr/nsurget/nicolasexamspringboot/NicolasExamSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nsurget.nicolasexamspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolasExamSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
